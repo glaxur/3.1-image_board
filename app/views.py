700bf1f8e20e0a677114f57131829ab5c525fbd9
Isaiah Santillan <isaiahsantillan111@icloud.com>
2019-01-23 10:52:56 -0500

    First commit

---
+++ app/views.py
@@ -0,0 +1,15 @@
+from django.views.generic import TemplateView
+from .models import Image
+
+
+class Board(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        images = Image.objects.all()
+
+        context = {
+            'images': images
+        }
+
+        return context
